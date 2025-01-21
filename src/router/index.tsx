b1f02dec4ce9364ee2ffd0088e1c49f385ebb3ed
hejp <378540660@qq.com>
2025-01-21 10:47:44 +0800

    feat:  增加路由

---
+++ src/router/index.tsx
@@ -0,0 +1,84 @@
+import React, { Suspense, FC, memo } from "react";
+import { Routes, Route, Navigate } from "react-router-dom";
+import routes, { IRoute, IMeta } from "./routes";
+import lazyLoad from "@src/compoents/lazyLoad";
+
+interface IPrivateRoute {
+  children: JSX.Element | null;
+  meta: IMeta;
+  title: string;
+  token: string;
+}
+
+const PrivateRoute: FC<IPrivateRoute> = ({ children, meta, title, token }) => {
+  if (title) {
+    document.title = `${window.CONFIG.title}-${title}`;
+  }
+  // 处理未登录情况时跳首页
+  if (meta.auth && !token) {
+    return <Navigate to="/login" />;
+  }
+  return children;
+};
+
+/**
+ * 递归路由
+ * @param datas 路由数据
+ * @returns
+ */
+const routeTree = (datas: IRoute[]) => {
+  return datas.map(({ path, children, modulePath, title, meta, redirect }) => {
+    return children && children.length ? (
+      <Route
+        path={path}
+        element={
+          modulePath ? (
+            <PrivateRoute title={title} meta={meta} token="">
+              {lazyLoad(modulePath)}
+            </PrivateRoute>
+          ) : null
+        }
+        key={modulePath}
+      >
+        {routeTree(children)}
+        {redirect ? (
+          <Route path={path} element={<Navigate to={redirect} />}></Route>
+        ) : (
+          <Route
+            path={path}
+            element={<Navigate to={children[0].path as string} />}
+          ></Route>
+        )}
+      </Route>
+    ) : (
+      <Route
+        path={path}
+        element={
+          (path as string) === "*" ? (
+            <Navigate to="/404" replace />
+          ) : modulePath ? (
+            <PrivateRoute title={title} meta={meta} token="">
+              {lazyLoad(modulePath)}
+            </PrivateRoute>
+          ) : null
+        }
+        key={modulePath}
+      ></Route>
+    );
+  });
+};
+
+const RoutesView = memo(
+  () => {
+    return (
+      <Suspense>
+        <Routes>{routeTree(routes)}</Routes>
+      </Suspense>
+    );
+  },
+  (a, b) => {
+    return true;
+  }
+);
+
+export default RoutesView;
---
+++ src/router/routes.ts
@@ -0,0 +1,68 @@
+import { NonIndexRouteObject } from "react-router-dom";
+
+export interface IMeta {
+  // 是否需要token
+  auth: boolean;
+  // 是否全屏
+  fullScreen?: boolean;
+  // 是否是菜单
+  menu?: boolean;
+}
+
+export interface IRoute extends NonIndexRouteObject {
+  // 页面标题
+  title: string;
+  // 模块路径在template模块下的路径
+  modulePath: string;
+  meta: IMeta;
+  // 子路由
+  children?: IRoute[];
+  // 访问的兄弟路由不存在时，重定向到该路由
+  redirect?: string;
+  state?: any;
+  icon?: string;
+}
+
+const routerDatas: IRoute[] = [
+  {
+    path: "/",
+    title: "",
+    modulePath: "layout",
+    meta: {
+      auth: false,
+      fullScreen: true,
+    },
+    children: [
+      {
+        path: "/login",
+        title: "登录",
+        redirect: "/login",
+        modulePath: "login",
+        meta: {
+          auth: false,
+          fullScreen: true,
+          menu: false,
+        },
+      },
+      {
+        path: "/404",
+        title: "404",
+        modulePath: "notFound",
+        meta: {
+          auth: false,
+          menu: false,
+        },
+      },
+      {
+        path: "*",
+        title: "404",
+        modulePath: "",
+        meta: {
+          auth: false,
+        },
+      },
+    ],
+  },
+];
+
+export default routerDatas;
