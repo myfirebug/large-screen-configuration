cedd1bdbb95fe877a00a21b6a625a0a815720b9c
hejp <378540660@qq.com>
2025-01-29 23:07:34 +0800

    feat: 增加组件接口

--- src/service/module/index.ts
+++ src/service/module/index.ts
@@ -1,4 +1,5 @@
 import loginService from "./login.service";
 import userService from "./user.service";
+import elementsService from "./elements.service";
 
-export const API = { loginService, userService };
+export const API = { loginService, userService, elementsService };
