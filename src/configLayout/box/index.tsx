0dec110c5257be7e4bc31b46b8bc37f2682bc575
hejp <378540660@qq.com>
2025-01-30 10:09:27 +0800

    feat:  增加配置盒子组件

---
+++ src/configLayout/box/index.tsx
@@ -0,0 +1,22 @@
+import React, { FC, ReactNode } from "react";
+import "./index.scss";
+import { CloseOutlined } from "@ant-design/icons";
+interface IBox {
+  title: string;
+  children: ReactNode;
+  onClose: () => void;
+}
+
+const Box: FC<IBox> = ({ title, children, onClose }) => {
+  return (
+    <div className="cms-box" style={{ display: title ? "block" : "none" }}>
+      <div className="cms-box__header">
+        <div className="name">{title}</div>
+        <CloseOutlined onClick={onClose} />
+      </div>
+      <div className="cms-box__body">{children}</div>
+    </div>
+  );
+};
+
+export default Box;
