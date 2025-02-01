0000c9f804fb49c9476cac607f466ed11d3f6cc2
hejp <378540660@qq.com>
2025-02-01 12:58:11 +0800

    feat:  增加微件列表接口数据

---
+++ src/mock/widgets.mock.ts
@@ -0,0 +1,18 @@
+export const widgets = {
+  url: "/widgets",
+  method: "get",
+  data: {
+    result: true,
+    data: [
+      {
+        id: 1,
+        name: "基础文本框",
+        url: "",
+        type: "text",
+        createTime: "",
+        count: 1000,
+      },
+    ],
+    message: "获取组建列表成功",
+  },
+};
