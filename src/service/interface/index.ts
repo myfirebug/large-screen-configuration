bdd70e6a36c0320a4e23a325a183b1298274afba
hejp <378540660@qq.com>
2025-02-01 12:57:09 +0800

    feat:  增加微件列表接口

--- src/service/interface/index.ts
+++ src/service/interface/index.ts
@@ -1,3 +1,4 @@
 export * from "./login.interface";
 export * from "./user.interface";
 export * from "./elements.interface";
+export * from "./widgets.interface";
--- src/service/module/index.ts
+++ src/service/module/index.ts
@@ -1,5 +1,11 @@
 import loginService from "./login.service";
 import userService from "./user.service";
 import elementsService from "./elements.service";
+import widgetsService from "./widgets.service";
 
-export const API = { loginService, userService, elementsService };
+export const API = {
+  loginService,
+  userService,
+  elementsService,
+  widgetsService,
+};
