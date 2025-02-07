ac911d452414939b8a363ce89cee97c773be8b40
hejp <378540660@qq.com>
2025-02-07 10:59:07 +0800

    fix:  修改显示组建逻辑

--- src/compoents/dragdrop/previewItem/index.tsx
+++ src/compoents/dragdrop/previewItem/index.tsx
@@ -1,9 +1,9 @@
 import React, {
-  FC,
   HtmlHTMLAttributes,
   useCallback,
   useMemo,
   useState,
+  memo,
 } from "react";
 import "./index.scss";
 import { dragStore } from "../drag";
@@ -15,89 +15,92 @@ interface IPreviewItem extends HtmlHTMLAttributes<HTMLDivElement> {
   groupName?: string;
 }
 
-const PreviewItem: FC<IPreviewItem> = ({
-  data,
-  style,
-  groupName,
-  onResizeStart,
-  onResizeing,
-  onResizeEnd,
-}) => {
-  const [moveing, setMoveing] = useState(false);
-  const [resizeing, setResizeing] = useState(false);
-  const previewStyles = useMemo(() => {
-    return {
-      gridArea: `${data.y} / ${data.x} / ${data.y + data.row}/ ${
-        data.x + data.column
-      }`,
-      ...(moveing
-        ? {
-            opacity: 0,
-            // 将当前元素移出容器外,将元素占位空置出来
-            transform: `translate(-999999999px, -9999999999px)`,
-          }
-        : {}),
-      ...(resizeing ? { opacity: 0.5 } : {}),
-    };
-  }, [data.column, data.row, data.x, data.y, moveing, resizeing]);
-  // 移动开始
-  const onDragStart = useCallback(
-    (e: React.DragEvent<HTMLDivElement>) => {
-      dragStore.set(groupName as string, data);
-      setTimeout(() => {
-        setMoveing(true);
-      }, 0);
-      unset(e.target);
-    },
-    [data, groupName]
-  );
-  // 移动结束
-  const onDragEnd = useCallback(
-    (e: React.DragEvent<HTMLDivElement>) => {
-      setMoveing(false);
-      dragStore.remove(groupName as string);
-    },
-    [groupName]
-  );
-
-  const onMouseDown = useCallback(
-    (e: React.DragEvent<HTMLDivElement>) => {
-      dragStore.set(groupName as string, data);
-      setResizeing(true);
-      onResizeStart?.();
-
-      e.target.addEventListener("mousemove", (event) => {
-        onResizeing?.(e);
-      });
-      e.target.addEventListener("mouseup", (event: any) => {
-        unset(event.target);
-        onResizeEnd?.();
-        event.target.style.width = "100%";
-        event.target.style.height = "100%";
+const PreviewItem = memo(
+  (props: IPreviewItem) => {
+    const { data, style, groupName, onResizeStart, onResizeing, onResizeEnd } =
+      props;
+    const [moveing, setMoveing] = useState(false);
+    const [resizeing, setResizeing] = useState(false);
+    const previewStyles = useMemo(() => {
+      return {
+        gridArea: `${data.y} / ${data.x} / ${data.y + data.row}/ ${
+          data.x + data.column
+        }`,
+        ...(moveing
+          ? {
+              opacity: 0,
+              // 将当前元素移出容器外,将元素占位空置出来
+              transform: `translate(-999999999px, -9999999999px)`,
+            }
+          : {}),
+        ...(resizeing ? { opacity: 0.5 } : {}),
+      };
+    }, [data.column, data.row, data.x, data.y, moveing, resizeing]);
+    // 移动开始
+    const onDragStart = useCallback(
+      (e: React.DragEvent<HTMLDivElement>) => {
+        dragStore.set(groupName as string, data);
+        setTimeout(() => {
+          setMoveing(true);
+        }, 0);
+        unset(e.target);
+      },
+      [data, groupName]
+    );
+    // 移动结束
+    const onDragEnd = useCallback(
+      (e: React.DragEvent<HTMLDivElement>) => {
+        setMoveing(false);
         dragStore.remove(groupName as string);
-      });
-    },
-    [data, groupName, onResizeEnd, onResizeStart, onResizeing]
-  );
+      },
+      [groupName]
+    );
 
-  const unset = (target: any) => {
-    setResizeing(false);
-    target.onmousemove = null;
-    target.onmouseup = null;
-  };
-  return (
-    <div
-      className="cms-drag__preview--item"
-      style={{ ...previewStyles, ...style }}
-      onDragStart={onDragStart}
-      draggable
-      onDragEnd={onDragEnd}
-      onMouseDown={onMouseDown}
-    >
-      <div className="close"></div>
-      <div className="resize"></div>
-    </div>
-  );
-};
+    const onMouseDown = useCallback(
+      (e: React.DragEvent<HTMLDivElement>) => {
+        dragStore.set(groupName as string, data);
+        setResizeing(true);
+        onResizeStart?.();
+
+        e.target.addEventListener("mousemove", (event) => {
+          onResizeing?.(e);
+        });
+        e.target.addEventListener("mouseup", (event: any) => {
+          unset(event.target);
+          onResizeEnd?.();
+          event.target.style.width = "100%";
+          event.target.style.height = "100%";
+          dragStore.remove(groupName as string);
+        });
+      },
+      [data, groupName, onResizeEnd, onResizeStart, onResizeing]
+    );
+
+    const unset = (target: any) => {
+      setResizeing(false);
+      target.onmousemove = null;
+      target.onmouseup = null;
+    };
+    return (
+      <div
+        className="cms-drag__preview--item"
+        style={{ ...previewStyles, ...style }}
+        onDragStart={onDragStart}
+        draggable
+        onDragEnd={onDragEnd}
+        onMouseDown={onMouseDown}
+      >
+        <div className="close"></div>
+        <div className="resize"></div>
+      </div>
+    );
+  },
+  (prevProps, nextProps) => {
+    if (JSON.stringify(prevProps) === JSON.stringify(nextProps)) {
+      return true;
+    }
+    return false;
+  }
+);
 
 export default PreviewItem;
