0647edb5761f9b89f2c77795fb768c4f19339241
hejp <378540660@qq.com>
2025-02-08 10:18:26 +0800

    fix:  增加configLayout组件导出总入口

---
+++ src/layout/configLayout/index.ts
@@ -0,0 +1,6 @@
+export * from "./components/header";
+export * from "./components/main";
+export * from "./components/leftAside";
+export * from "./components/leftAside/components/elements";
+export * from "./components/rightAside";
+export * from "./components/rightAside/components/layer";
