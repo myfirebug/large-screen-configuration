73a0f09d3a231a85a01f6cd8b9782b71645ca5d4
hejp <378540660@qq.com>
2025-02-08 10:14:56 +0800

    fix:  修改展示组件&拖拽组件逻辑

--- src/compoents/dragdrop/dragContent/index.tsx
+++ src/compoents/dragdrop/dragContent/index.tsx
@@ -1,5 +1,6 @@
 import React, {
   memo,
+  ReactNode,
   useCallback,
   useEffect,
   useMemo,
@@ -42,308 +43,298 @@ interface IDragContent {
    * @returns
    */
   onCloseHander?: (id: string) => void;
+
+  renderPreview?: (data: IAnyObject) => ReactNode;
 }
 
-const DragContent = memo(
-  (props: IDragContent) => {
-    const {
-      groupName,
-      column,
-      row,
-      gap,
-      mask = true,
-      field = "id",
-      onDropHandler,
-      datas,
-      onResizeEndHandler,
-      onCloseHander,
-    } = props;
-    // 目标元素
-    const target = useRef<HTMLDivElement>(null);
-    // 宽高
-    const [clientRect, setClientRect] = useState({
-      width: 0,
-      height: 0,
-      x: 0,
-      y: 0,
+const DragContent = memo((props: IDragContent) => {
+  const {
+    groupName,
+    column,
+    row,
+    gap,
+    mask = true,
+    field = "id",
+    onDropHandler,
+    datas,
+    onResizeEndHandler,
+    onCloseHander,
+    renderPreview,
+  } = props;
+  // 目标元素
+  const target = useRef<HTMLDivElement>(null);
+  // 宽高
+  const [clientRect, setClientRect] = useState({
+    width: 0,
+    height: 0,
+    x: 0,
+    y: 0,
+  });
+  const [resizeEnd, setResizeEnd] = useState(false);
+  // 拖拽中的元素
+  const [current, setCuurent] = useState<IAnyObject>({});
+  // 动态计算目标元素的宽高
+  const getClientRect = useCallback(() => {
+    if (!target.current) return;
+    const { width, height, x, y } = (
+      target.current as HTMLDivElement
+    )?.getBoundingClientRect();
+    setClientRect({
+      width: (width - (column - 1) * gap) / column,
+      height: (height - (row - 1) * gap) / row,
+      x: x,
+      y: y,
     });
-    const [resizeEnd, setResizeEnd] = useState(false);
-    // 拖拽中的元素
-    const [current, setCuurent] = useState<IAnyObject>({});
-    // 动态计算目标元素的宽高
-    const getClientRect = useCallback(() => {
-      if (!target.current) return;
-      const { width, height, x, y } = (
-        target.current as HTMLDivElement
-      )?.getBoundingClientRect();
-      setClientRect({
-        width: (width - (column - 1) * gap) / column,
-        height: (height - (row - 1) * gap) / row,
-        x: x,
-        y: y,
-      });
-    }, [column, gap, row]);
-    useEffect(() => {
+  }, [column, gap, row]);
+  useEffect(() => {
+    getClientRect();
+    const resizeObserver = new ResizeObserver((entries) => {
+      if (!Array.isArray(entries) || !entries.length) {
+        return;
+      }
       getClientRect();
-      const resizeObserver = new ResizeObserver((entries) => {
-        if (!Array.isArray(entries) || !entries.length) {
-          return;
-        }
-        getClientRect();
-      });
+    });
 
-      resizeObserver.observe(target.current as HTMLDivElement);
-      return () => {
-        resizeObserver.disconnect();
-      };
-    }, [getClientRect]);
-    // 判断是否可放置
-    const isPutDown = useMemo(() => {
-      const currentXy: [number, number, number, number] = [
-        current.x,
-        current.y,
-        current.x + current.column - 1,
-        current.y + current.row - 1,
-      ];
-      return (
-        booleanWithin([0, 0, column, row], currentXy) &&
-        datas.every(
-          (item) =>
-            item[field] === current[field] ||
-            !booleanIntersects(
-              [item.x - 1, item.y - 1, item.x + item.column, item.y + item.row],
-              currentXy
-            )
-        )
-      );
-    }, [column, current, datas, field, row]);
+    resizeObserver.observe(target.current as HTMLDivElement);
+    return () => {
+      resizeObserver.disconnect();
+    };
+  }, [getClientRect]);
+  // 判断是否可放置
+  const isPutDown = useMemo(() => {
+    const currentXy: [number, number, number, number] = [
+      current.x,
+      current.y,
+      current.x + current.column - 1,
+      current.y + current.row - 1,
+    ];
+    return (
+      booleanWithin([0, 0, column, row], currentXy) &&
+      datas.every(
+        (item) =>
+          item[field] === current[field] ||
+          !booleanIntersects(
+            [item.x - 1, item.y - 1, item.x + item.column, item.y + item.row],
+            currentXy
+          )
+      )
+    );
+  }, [column, current, datas, field, row]);
 
-    // 计算 x 坐标
-    const getX = useCallback(
-      (num: number) =>
-        parseInt(String((num - clientRect.x) / (clientRect.width + gap))) + 1,
-      [clientRect.width, clientRect.x, gap]
-    );
-    // 计算 y 坐标
-    const getY = useCallback(
-      (num: number) =>
-        parseInt(String((num - clientRect.y) / (clientRect.height + gap))) + 1,
-      [clientRect.height, clientRect.y, gap]
-    );
-    // 计算列数
-    const getColumn = useCallback(
-      (num: number) => Math.max(1, Math.ceil(num / (clientRect.width + gap))),
-      [clientRect.width, gap]
-    );
-    // 计算行数
-    const getRow = useCallback(
-      (num: number) => Math.max(1, Math.ceil(num / (clientRect.height + gap))),
-      [clientRect.height, gap]
-    );
-    // 进入放置目标
-    const onDragEnter = useCallback(
-      (e: React.DragEvent<HTMLDivElement>) => {
-        e.preventDefault();
-        const dragData = dragStore.get(groupName as string);
-        if (dragData) {
-          setCuurent((state) => ({
-            ...state,
-            ...dragData,
-            x: getX(e.pageX),
-            y: getY(e.pageY),
-            show: true,
-          }));
-        }
-        console.log("onDragEnter");
-      },
-      [getX, getY, groupName]
-    );
-    // 在目标中移动
-    const onDragOver = useCallback(
-      (e: React.DragEvent<HTMLDivElement>) => {
-        e.preventDefault();
-        const dragData = dragStore.get(groupName as string);
-        if (dragData) {
-          setCuurent((state) => ({
-            ...state,
-            ...dragData,
-            x: getX(e.pageX),
-            y: getY(e.pageY),
-            show: true,
-          }));
-        }
-        console.log("onDragOver");
-      },
-      [getX, getY, groupName]
-    );
-    // 离开目标
-    const onDragLeave = useCallback(
-      (e: React.DragEvent<HTMLDivElement>) => {
-        e.preventDefault();
-        setCuurent((state) => ({
-          ...state,
-          show: false,
-          [field]: undefined,
-        }));
-        console.log("onDragLeave");
-      },
-      [field]
-    );
-    // 放置在目标上
-    const onDrop = useCallback(
-      (e: React.DragEvent<HTMLDivElement>) => {
-        const dragData = dragStore.get(groupName as string);
-        e.preventDefault();
-        setCuurent((state) => ({
-          ...state,
-          show: false,
-        }));
-        if (isPutDown) {
-          onDropHandler &&
-            onDropHandler(!Boolean(dragData && dragData[field]), {
-              ...dragData,
-              ...current,
-              [field]: dragData && dragData[field] ? dragData[field] : guid(),
-            });
-        }
-        console.log("onDrop");
-      },
-      [current, field, groupName, isPutDown, onDropHandler]
-    );
-    // 调整大小开始
-    const onResizeStart = useCallback(() => {
+  // 计算 x 坐标
+  const getX = useCallback(
+    (num: number) =>
+      parseInt(String((num - clientRect.x) / (clientRect.width + gap))) + 1,
+    [clientRect.width, clientRect.x, gap]
+  );
+  // 计算 y 坐标
+  const getY = useCallback(
+    (num: number) =>
+      parseInt(String((num - clientRect.y) / (clientRect.height + gap))) + 1,
+    [clientRect.height, clientRect.y, gap]
+  );
+  // 计算列数
+  const getColumn = useCallback(
+    (num: number) => Math.max(1, Math.ceil(num / (clientRect.width + gap))),
+    [clientRect.width, gap]
+  );
+  // 计算行数
+  const getRow = useCallback(
+    (num: number) => Math.max(1, Math.ceil(num / (clientRect.height + gap))),
+    [clientRect.height, gap]
+  );
+  // 进入放置目标
+  const onDragEnter = useCallback(
+    (e: React.DragEvent<HTMLDivElement>) => {
+      e.preventDefault();
       const dragData = dragStore.get(groupName as string);
       if (dragData) {
-        setResizeEnd(false);
         setCuurent((state) => ({
           ...state,
           ...dragData,
+          x: getX(e.pageX),
+          y: getY(e.pageY),
           show: true,
         }));
       }
-    }, [groupName]);
-    // 调正大小时
-    const onResizeing = useCallback(
-      (e: any) => {
-        setResizeEnd(false);
+      console.log("onDragEnter");
+    },
+    [getX, getY, groupName]
+  );
+  // 在目标中移动
+  const onDragOver = useCallback(
+    (e: React.DragEvent<HTMLDivElement>) => {
+      e.preventDefault();
+      const dragData = dragStore.get(groupName as string);
+      if (dragData) {
         setCuurent((state) => ({
           ...state,
-          column: getColumn(e.target.offsetWidth),
-          row: getRow(e.target.offsetHeight),
+          ...dragData,
+          x: getX(e.pageX),
+          y: getY(e.pageY),
+          show: true,
         }));
-      },
-      [getColumn, getRow]
-    );
-    // 调整大小结束
-    const onResizeEnd = useCallback(() => {
-      setResizeEnd(true);
+      }
+      console.log("onDragOver");
+    },
+    [getX, getY, groupName]
+  );
+  // 离开目标
+  const onDragLeave = useCallback(
+    (e: React.DragEvent<HTMLDivElement>) => {
+      e.preventDefault();
+      setCuurent((state) => ({
+        ...state,
+        show: false,
+        [field]: undefined,
+      }));
+      console.log("onDragLeave");
+    },
+    [field]
+  );
+  // 放置在目标上
+  const onDrop = useCallback(
+    (e: React.DragEvent<HTMLDivElement>) => {
+      const dragData = dragStore.get(groupName as string);
+      e.preventDefault();
       setCuurent((state) => ({
         ...state,
         show: false,
       }));
-    }, []);
-
-    useEffect(() => {
-      const dragData = dragStore.get(groupName as string);
-      if (resizeEnd && isPutDown) {
-        if (
-          dragData?.column !== current.column ||
-          dragData?.row !== current.row
-        ) {
-          console.log(
-            dragData,
-            current,
-            groupName,
-            "update onResizeEndHandler"
-          );
-          onResizeEndHandler?.(current);
-          setResizeEnd(false);
-        }
+      if (isPutDown) {
+        onDropHandler &&
+          onDropHandler(!Boolean(dragData && dragData[field]), {
+            ...dragData,
+            ...current,
+            [field]: dragData && dragData[field] ? dragData[field] : guid(),
+          });
       }
-    }, [onResizeEndHandler, current, groupName, resizeEnd, isPutDown]);
-
-    return (
-      <div
-        ref={target}
-        className="cms-drag__content"
-        onDragEnter={onDragEnter}
-        onDragOver={onDragOver}
-        onDragLeave={onDragLeave}
-        onDrop={onDrop}
-      >
-        <div
-          className="cms-drag__content--container"
-          style={{
-            display: "grid",
-            gap: `${gap}px`,
-            gridTemplateColumns: `repeat(${column}, ${clientRect.width}px)`,
-            gridTemplateRows: `repeat(${row},  ${clientRect.height}px)`,
-            width: "100%",
-            height: "100%",
-          }}
-        >
-          {new Array(column * row).fill(null).map((_, index) => (
-            <div key={index}></div>
-          ))}
-        </div>
-        <div
-          className="cms-drag__preview"
-          style={{
-            display: "grid",
-            gap: `${gap}px`,
-            gridTemplateColumns: `repeat(${column}, ${clientRect.width}px)`,
-            gridTemplateRows: `repeat(${row},  ${clientRect.height}px)`,
-            width: "100%",
-            height: "100%",
-          }}
-        >
-          {datas.map((item) => (
-            <PreviewItem
-              key={item[field]}
-              data={item}
-              groupName={groupName}
-              style={{
-                pointerEvents:
-                  current.show && item[field] !== current[field]
-                    ? "none"
-                    : "all",
-              }}
-              onResizeStart={onResizeStart}
-              onResizeing={onResizeing}
-              onResizeEnd={onResizeEnd}
-              onCloseHander={onCloseHander}
-            ></PreviewItem>
-          ))}
-
-          {mask ? (
-            <MoveMask
-              style={{
-                display: current?.show ? "flex" : "none",
-                background: isPutDown
-                  ? "var(--cms-divider-color)"
-                  : "var(--cms-color-help)",
-              }}
-              width={clientRect.width}
-              height={clientRect.height}
-              gap={gap}
-              column={current?.column}
-              row={current?.row}
-              x={current?.x}
-              y={current?.y}
-            >
-              {isPutDown ? "可放置" : "不可放置"}
-            </MoveMask>
-          ) : null}
-        </div>
-      </div>
-    );
-  },
-  (prevProps, nextProps) => {
-    if (JSON.stringify(prevProps) === JSON.stringify(nextProps)) {
-      return true;
+      console.log("onDrop");
+    },
+    [current, field, groupName, isPutDown, onDropHandler]
+  );
+  // 调整大小开始
+  const onResizeStart = useCallback(() => {
+    const dragData = dragStore.get(groupName as string);
+    if (dragData) {
+      setResizeEnd(false);
+      setCuurent((state) => ({
+        ...state,
+        ...dragData,
+        show: true,
+      }));
     }
-    return false;
-  }
-);
+  }, [groupName]);
+  // 调正大小时
+  const onResizeing = useCallback(
+    (e: any) => {
+      setResizeEnd(false);
+      setCuurent((state) => ({
+        ...state,
+        column: getColumn(e.target.offsetWidth),
+        row: getRow(e.target.offsetHeight),
+      }));
+    },
+    [getColumn, getRow]
+  );
+  // 调整大小结束
+  const onResizeEnd = useCallback(() => {
+    setResizeEnd(true);
+    setCuurent((state) => ({
+      ...state,
+      show: false,
+    }));
+  }, []);
+
+  useEffect(() => {
+    const dragData = dragStore.get(groupName as string);
+    if (resizeEnd && isPutDown) {
+      if (
+        dragData?.column !== current.column ||
+        dragData?.row !== current.row
+      ) {
+        console.log(dragData, current, groupName, "update onResizeEndHandler");
+        onResizeEndHandler?.(current);
+        setResizeEnd(false);
+      }
+    }
+  }, [onResizeEndHandler, current, groupName, resizeEnd, isPutDown]);
+
+  return (
+    <div
+      ref={target}
+      className="cms-drag__content"
+      onDragEnter={onDragEnter}
+      onDragOver={onDragOver}
+      onDragLeave={onDragLeave}
+      onDrop={onDrop}
+    >
+      <div
+        className="cms-drag__content--container"
+        style={{
+          display: "grid",
+          gap: `${gap}px`,
+          gridTemplateColumns: `repeat(${column}, ${clientRect.width}px)`,
+          gridTemplateRows: `repeat(${row},  ${clientRect.height}px)`,
+          width: "100%",
+          height: "100%",
+        }}
+      >
+        {new Array(column * row).fill(null).map((_, index) => (
+          <div key={index}></div>
+        ))}
+      </div>
+      <div
+        className="cms-drag__preview"
+        style={{
+          display: "grid",
+          gap: `${gap}px`,
+          gridTemplateColumns: `repeat(${column}, ${clientRect.width}px)`,
+          gridTemplateRows: `repeat(${row},  ${clientRect.height}px)`,
+          width: "100%",
+          height: "100%",
+        }}
+      >
+        {datas.map((item) => (
+          <PreviewItem
+            key={item[field]}
+            data={item}
+            groupName={groupName}
+            style={{
+              pointerEvents:
+                current.show && item[field] !== current[field] ? "none" : "all",
+            }}
+            onResizeStart={onResizeStart}
+            onResizeing={onResizeing}
+            onResizeEnd={onResizeEnd}
+            onCloseHander={onCloseHander}
+          >
+            {renderPreview?.(item)}
+          </PreviewItem>
+        ))}
+
+        {mask ? (
+          <MoveMask
+            style={{
+              display: current?.show ? "flex" : "none",
+              background: isPutDown
+                ? "var(--cms-divider-color)"
+                : "var(--cms-color-help)",
+            }}
+            width={clientRect.width}
+            height={clientRect.height}
+            gap={gap}
+            column={current?.column}
+            row={current?.row}
+            x={current?.x}
+            y={current?.y}
+          >
+            {isPutDown ? "可放置" : "不可放置"}
+          </MoveMask>
+        ) : null}
+      </div>
+    </div>
+  );
+});
 
 export default DragContent;
--- src/compoents/dragdrop/previewItem/index.tsx
+++ src/compoents/dragdrop/previewItem/index.tsx
@@ -4,6 +4,7 @@ import React, {
   useMemo,
   useState,
   memo,
+  ReactNode,
 } from "react";
 import "./index.scss";
 import { dragStore } from "../drag";
@@ -18,113 +19,109 @@ interface IPreviewItem extends HtmlHTMLAttributes<HTMLDivElement> {
    * @returns
    */
   onCloseHander?: (id: string) => void;
+  children: ReactNode;
 }
 
-const PreviewItem = memo(
-  (props: IPreviewItem) => {
-    const {
-      data,
-      style,
-      groupName,
-      onResizeStart,
-      onResizeing,
-      onResizeEnd,
-      onCloseHander,
-    } = props;
-    const [moveing, setMoveing] = useState(false);
-    const [resizeing, setResizeing] = useState(false);
-    const previewStyles = useMemo(() => {
-      return {
-        gridArea: `${data.y} / ${data.x} / ${data.y + data.row}/ ${
-          data.x + data.column
-        }`,
-        ...(moveing
-          ? {
-              opacity: 0,
-              // 将当前元素移出容器外,将元素占位空置出来
-              transform: `translate(-999999999px, -9999999999px)`,
-            }
-          : {}),
-        ...(resizeing ? { opacity: 0.5 } : {}),
-      };
-    }, [data.column, data.row, data.x, data.y, moveing, resizeing]);
-    // 移动开始
-    const onDragStart = useCallback(
-      (e: React.DragEvent<HTMLDivElement>) => {
-        dragStore.set(groupName as string, {
-          ...data,
-          pageX: e.pageX,
-          pageY: e.pageY,
-        });
-        setTimeout(() => {
-          setMoveing(true);
-        }, 0);
-        setResizeing(false);
-      },
-      [data, groupName]
-    );
-    // 移动结束
-    const onDragEnd = useCallback(
-      (e: React.DragEvent<HTMLDivElement>) => {
-        setMoveing(false);
-        dragStore.remove(groupName as string);
-      },
-      [groupName]
-    );
+const PreviewItem = memo((props: IPreviewItem) => {
+  const {
+    data,
+    style,
+    groupName,
+    onResizeStart,
+    onResizeing,
+    onResizeEnd,
+    onCloseHander,
+    children,
+  } = props;
+  const [moveing, setMoveing] = useState(false);
+  const [resizeing, setResizeing] = useState(false);
+  const previewStyles = useMemo(() => {
+    return {
+      gridArea: `${data.y} / ${data.x} / ${data.y + data.row}/ ${
+        data.x + data.column
+      }`,
+      ...(moveing
+        ? {
+            opacity: 0,
+            // 将当前元素移出容器外,将元素占位空置出来
+            transform: `translate(-999999999px, -9999999999px)`,
+          }
+        : {}),
+      ...(resizeing ? { opacity: 0.5 } : {}),
+    };
+  }, [data.column, data.row, data.x, data.y, moveing, resizeing]);
+  // 移动开始
+  const onDragStart = useCallback(
+    (e: React.DragEvent<HTMLDivElement>) => {
+      dragStore.set(groupName as string, {
+        ...data,
+        pageX: e.pageX,
+        pageY: e.pageY,
+      });
+      setTimeout(() => {
+        setMoveing(true);
+      }, 0);
+      setResizeing(false);
+    },
+    [data, groupName]
+  );
+  // 移动结束
+  const onDragEnd = useCallback(
+    (e: React.DragEvent<HTMLDivElement>) => {
+      setMoveing(false);
+      dragStore.remove(groupName as string);
+    },
+    [groupName]
+  );
 
-    const onMouseDown = useCallback(
-      (e: React.DragEvent<HTMLDivElement>) => {
-        dragStore.set(groupName as string, data);
-        setResizeing(true);
-        onResizeStart?.();
-      },
-      [data, groupName, onResizeStart]
-    );
+  const onMouseDown = useCallback(
+    (e: React.DragEvent<HTMLDivElement>) => {
+      dragStore.set(groupName as string, data);
+      setResizeing(true);
+      onResizeStart?.();
+    },
+    [data, groupName, onResizeStart]
+  );
 
-    const onMouseMove = useCallback(
-      (e: React.DragEvent<HTMLDivElement>) => {
-        onResizeing?.(e);
-      },
-      [onResizeing]
-    );
+  const onMouseMove = useCallback(
+    (e: React.DragEvent<HTMLDivElement>) => {
+      e.preventDefault();
+      onResizeing?.(e);
+    },
+    [onResizeing]
+  );
 
-    const onMouseUp = useCallback(
-      (e: any) => {
-        onResizeEnd?.();
-        e.target.style.width = "100%";
-        e.target.style.height = "100%";
-        setResizeing(false);
-      },
-      [onResizeEnd]
-    );
-    return (
+  const onMouseUp = useCallback(
+    (e: any) => {
+      onResizeEnd?.();
+      e.target.style.width = "100%";
+      e.target.style.height = "100%";
+      setResizeing(false);
+    },
+    [onResizeEnd]
+  );
+  return (
+    <div
+      className="cms-drag__preview--item"
+      style={{ ...previewStyles, ...style }}
+      onDragStart={onDragStart}
+      draggable
+      onDragEnd={onDragEnd}
+      onMouseDown={onMouseDown}
+      onMouseMove={onMouseMove}
+      onMouseUp={onMouseUp}
+    >
+      {children}
       <div
-        className="cms-drag__preview--item"
-        style={{ ...previewStyles, ...style }}
-        onDragStart={onDragStart}
-        draggable
-        onDragEnd={onDragEnd}
-        onMouseDown={onMouseDown}
-        onMouseMove={onMouseMove}
-        onMouseUp={onMouseUp}
-      >
-        <div
-          className="close"
-          onClick={(e) => {
-            e.preventDefault();
-            onCloseHander?.(data.elementId);
-          }}
-        ></div>
-        <div className="resize"></div>
-      </div>
-    );
-  },
-  (prevProps, nextProps) => {
-    if (JSON.stringify(prevProps) === JSON.stringify(nextProps)) {
-      return true;
-    }
-    return false;
-  }
-);
+        className="close"
+        onClick={(e) => {
+          e.preventDefault();
+          onCloseHander?.(data.elementId);
+        }}
+      ></div>
+      <div className="resize"></div>
+    </div>
+  );
+});
 
 export default PreviewItem;
