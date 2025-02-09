fd87f59e6aae4b99ad79640bea46c8e729091ec9
hejp <378540660@qq.com>
2025-02-09 09:58:47 +0800

    feat:  增加动态配置表单组件

---
+++ src/layout/configLayout/components/rightAside/components/dynamicForm/index.tsx
@@ -0,0 +1,89 @@
+import { Collapse, Form } from "antd";
+import React, { FC } from "react";
+import BaseForm from "../baseForm";
+
+const { Panel } = Collapse;
+
+interface IDynamicForm {
+  datas: any;
+}
+
+// 判断数据是Array 或者 object
+const judgeType = (data: any, type: string) => {
+  return Object.prototype.toString.call(data) === type;
+};
+
+const DynamicForm: FC<IDynamicForm> = ({ datas }) => {
+  return datas.map((item: any, index: number) => {
+    if (judgeType(item, "[object Object]")) {
+      const relationFields =
+        item.relationFields !== undefined ? item.relationFields.split(",") : [];
+      return (
+        <div key={index}>
+          {!relationFields.length ? (
+            <BaseForm item={item} />
+          ) : (
+            <Form.Item noStyle shouldUpdate>
+              {({ getFieldValue }) => {
+                if (
+                  relationFields.every((subItem: string) =>
+                    item.relationValues.includes(String(getFieldValue(subItem)))
+                  )
+                ) {
+                  return <BaseForm item={item} />;
+                }
+              }}
+            </Form.Item>
+          )}
+        </div>
+      );
+    }
+    if (judgeType(item, "[object Array]")) {
+      return (
+        <div key={index}>
+          {item.map((subItem: any, subIndex: number) => {
+            const relationFields =
+              subItem.relationFields !== undefined
+                ? subItem.relationFields.split(",")
+                : [];
+            return (
+              <Collapse key={subIndex} size="small">
+                {subItem.relationFields === undefined ? (
+                  <Panel header={subItem.name} key={subItem + subIndex}>
+                    <DynamicForm datas={subItem.list} />
+                  </Panel>
+                ) : (
+                  <Form.Item noStyle shouldUpdate>
+                    {({ getFieldValue }) => {
+                      if (
+                        relationFields.every((subbItem: string) =>
+                          subItem.relationValues.includes(
+                            String(getFieldValue(subbItem))
+                          )
+                        )
+                      ) {
+                        return (
+                          <Collapse key={subIndex} size="small">
+                            <Panel
+                              header={subItem.name}
+                              key={subItem + subIndex}
+                            >
+                              <DynamicForm datas={subItem.list} />
+                            </Panel>
+                          </Collapse>
+                        );
+                      }
+                    }}
+                  </Form.Item>
+                )}
+              </Collapse>
+            );
+          })}
+        </div>
+      );
+    }
+    return null;
+  });
+};
+
+export default DynamicForm;
