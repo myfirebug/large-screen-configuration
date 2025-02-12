24c0867663c8ad9af0df7c7211154aa8db69a165
hejp <378540660@qq.com>
2025-02-12 20:10:47 +0800

    fix: 增加折线图配置

---
+++ src/core/config/classification/line/index.ts
@@ -0,0 +1,43 @@
+import { echartsConfig, animateConfig, dataConfig } from "../../base";
+
+export const baseLine = {
+  configure: [
+    [
+      {
+        name: "基础设置",
+        list: [
+          ...echartsConfig.line,
+          ...echartsConfig.seriesStack,
+          echartsConfig.seriesLabel,
+          echartsConfig.symbol,
+        ],
+      },
+      {
+        name: "标题",
+        list: echartsConfig.title,
+      },
+      {
+        name: "图例",
+        list: echartsConfig.legend,
+      },
+      {
+        name: "网格",
+        list: echartsConfig.grid,
+      },
+      {
+        name: "X轴",
+        list: echartsConfig.xAxis,
+      },
+      {
+        name: "y轴",
+        list: echartsConfig.yAxis,
+      },
+      {
+        name: "自定义颜色",
+        list: echartsConfig.echartColor,
+      },
+      ...animateConfig.configure,
+    ],
+  ],
+  data: dataConfig.configure,
+};
