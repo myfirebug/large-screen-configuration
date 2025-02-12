27d192cae8419fd379b3f49ec3f7c5da98d6e746
hejp <378540660@qq.com>
2025-02-12 20:07:45 +0800

    fix: 修改类型

--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -12,7 +12,14 @@ type elementsType = "text" | "image" | "table" | "line" | "bar" | "pie";
 // 组件名称类型
 type elementsNameType = elementsType | "baseText";
 // 配置名称类型
-type codeNameType = "baseText" | "baseLine" | "baseSmookLine";
+type codeNameType =
+  | "baseText"
+  | "baseLine"
+  | "baseSmoothLine"
+  | "baseAreaLine"
+  | "stackedLine"
+  | "stackedAreaLine"
+  | "lengthwaysLine";
 // 任意object类型
 interface IAnyObject {
   [propName: string]: any;
