89de2d0dcd1309ceec4ff1556c9bb10034b33a8e
hejp <378540660@qq.com>
2025-02-13 11:37:23 +0800

    fix: 修改文本配置内容

--- src/core/config/classification/text/base.ts
+++ src/core/config/classification/text/base.ts
@@ -5,42 +5,52 @@ export const baseText = {
   code: "baseText",
   // 基础配置项
   configure: [
-    ...fontConfig.configure,
     [
       {
-        name: "文字阴影",
+        name: "基础设置",
         list: [
-          {
-            componentName: "InputNumber",
-            label: "X轴偏移",
-            name: "styleTextShadowX",
-            required: false,
-            placeholder: "请输入X轴偏移",
-          },
-          {
-            componentName: "InputNumber",
-            label: "Y轴偏移",
-            name: "styleTextShadowY",
-            required: false,
-            placeholder: "请输入Y轴偏移",
-          },
-          {
-            componentName: "InputNumber",
-            label: "模糊值",
-            name: "styleTextShadowF",
-            required: false,
-            placeholder: "请输入模糊值",
-          },
-          {
-            componentName: "SketchPicker",
-            label: "颜色",
-            name: "styleTextShadowC",
-            required: false,
-            placeholder: "请选择颜色",
-          },
+          ...fontConfig.configure,
+          [
+            {
+              name: "文字阴影",
+              list: [
+                {
+                  componentName: "InputNumber",
+                  label: "X轴偏移",
+                  name: "styleTextShadowX",
+                  required: false,
+                  placeholder: "请输入X轴偏移",
+                  addonAfter: "px",
+                },
+                {
+                  componentName: "InputNumber",
+                  label: "Y轴偏移",
+                  name: "styleTextShadowY",
+                  required: false,
+                  placeholder: "请输入Y轴偏移",
+                  addonAfter: "px",
+                },
+                {
+                  componentName: "InputNumber",
+                  label: "模糊值",
+                  name: "styleTextShadowF",
+                  required: false,
+                  placeholder: "请输入模糊值",
+                  addonAfter: "px",
+                },
+                {
+                  componentName: "SketchPicker",
+                  label: "颜色",
+                  name: "styleTextShadowC",
+                  required: false,
+                  placeholder: "请选择颜色",
+                },
+              ],
+            },
+            ...boxConfig.configure,
+          ],
         ],
       },
-      ...boxConfig.configure,
     ],
   ],
   data: dataConfig.configure,
