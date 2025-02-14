56bbea9fd20cad19b98e1e89f8c3c89a2e47040d
hejp <378540660@qq.com>
2025-02-14 12:06:01 +0800

    feat:  增加基础柱状图配置

---
+++ src/core/config/elements/bar/bar.ts
@@ -0,0 +1,71 @@
+import { echartsConfig, animateConfig, dataConfig } from "../../base";
+
+const bar = {
+  type: "bar",
+  // 配置项值
+  configureValue: {
+    styleDisplay: "block",
+    ...animateConfig.configureValue,
+    ...echartsConfig.titleValue,
+    ...echartsConfig.legendValue,
+    ...echartsConfig.gridValue,
+    ...echartsConfig.xAxisValue,
+    ...echartsConfig.yAxisValue,
+    ...echartsConfig.echartColorValue,
+    ...echartsConfig.barValue,
+    ...echartsConfig.seriesLabelValue,
+    ...echartsConfig.seriesStackValue,
+    xAxisBoundaryGap: true,
+    yAxisBoundaryGap: true,
+  },
+  // 坐标值
+  coordinateValue: {
+    left: 0,
+    top: 0,
+    width: 467,
+    height: 346,
+  },
+  // 数据值
+  dataValue: {
+    ...dataConfig.configureValue,
+    field: "series",
+    mock: {
+      series: [
+        {
+          seriesName: "Email",
+          data: [
+            {
+              name: "Mon",
+              value: 120,
+            },
+            {
+              name: "Tue",
+              value: 132,
+            },
+            {
+              name: "Wed",
+              value: 101,
+            },
+            {
+              name: "Thu",
+              value: 134,
+            },
+            {
+              name: "Fri",
+              value: 90,
+            },
+            {
+              name: "Sat",
+              value: 230,
+            },
+            {
+              name: "Sun",
+              value: 210,
+            },
+          ],
+        },
+      ],
+    },
+  },
+};
+export default bar;
---
+++ src/core/config/elements/bar/base.ts
@@ -0,0 +1,6 @@
+import bar from "./bar";
+
+export const baseBar = {
+  code: "baseBar",
+  ...bar,
+};
