a3e845546d42c7b95e2dbb2985ea1bee56c2216e
hejp <378540660@qq.com>
2025-02-21 10:20:24 +0800

    feat:  增加广告图组件

---
+++ src/elements/image/banner/index.tsx
@@ -0,0 +1,36 @@
+import React, { memo, useMemo } from "react";
+import CustomSwiper from "@src/compoents/swiper";
+import "./index.scss";
+import { getStyles } from "@src/utils";
+interface IBaseBanner {
+  data: any;
+  field: string;
+  options: any;
+}
+export const BaseBanner = memo((option: IBaseBanner) => {
+  const { data, field, options } = option;
+  const baseTextStyle = useMemo(() => {
+    return getStyles(options);
+  }, [options]);
+  return (
+    <div className="cms-banner" style={baseTextStyle}>
+      <CustomSwiper
+        loop={options.loop || true}
+        autoplay={options.autoplay || true}
+        datas={data[field] || []}
+        rows={options.rows || 1}
+        slidesPerView={options.slidesPerView || 1}
+        spaceBetween={options.spaceBetween || 0}
+        pagination={options.pagination || false}
+        navigation={options.navigation || false}
+        render={(item) => {
+          return (
+            <img src={item.src} alt={item.name} width="100%" height="100%" />
+          );
+        }}
+      />
+    </div>
+  );
+});
+
+export default BaseBanner;
