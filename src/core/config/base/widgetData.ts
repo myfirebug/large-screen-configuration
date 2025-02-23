b951b927c0441711da235abc2812f2d875fc231f
hejp <378540660@qq.com>
2025-02-23 15:56:45 +0800

    fix:  修改微件配置项

--- src/core/config/base/widgetData.ts
+++ src/core/config/base/widgetData.ts
@@ -1,7 +1,6 @@
 export const widgetDataConfig = {
   // 数据项默认值
   configureValue: {
-    useInterface: false,
     dataType: "mock",
     mock: {
       value: "文本框",
@@ -13,30 +12,11 @@ export const widgetDataConfig = {
   // 数据项配置
   configure: [
     {
-      componentName: "Switch",
-      label: "使用微件数据",
-      name: "useInterface",
-      required: false,
-      placeholder: "",
-      tooltip: "该组件使用微件的接口数据",
-    },
-    {
-      componentName: "JsonEdit",
-      label: "模拟数据",
-      name: "mock",
-      required: false,
-      placeholder: "请输入mock数据",
-      relationFields: "useInterface",
-      relationValues: "false",
-    },
-    {
       componentName: "TextArea",
       label: "接口地址",
       name: "url",
       required: false,
       placeholder: "请输入接口地址",
-      relationFields: "useInterface",
-      relationValues: "true",
     },
     {
       componentName: "Select",
@@ -44,8 +24,6 @@ export const widgetDataConfig = {
       name: "method",
       required: false,
       placeholder: "",
-      relationFields: "useInterface",
-      relationValues: "true",
       options: [
         { code: "get", name: "GET" },
         { code: "post", name: "post" },
@@ -57,15 +35,6 @@ export const widgetDataConfig = {
       name: "params",
       required: false,
       placeholder: "请输入请求参数",
-      relationFields: "useInterface",
-      relationValues: "true",
-    },
-    {
-      componentName: "Input",
-      label: "对应字段",
-      name: "field",
-      required: false,
-      placeholder: "",
     },
   ],
 };
