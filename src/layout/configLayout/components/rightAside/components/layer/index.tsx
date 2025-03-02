e6d718aeb567c4f00f5f90fc1546dc43a95ef321
hejp <378540660@qq.com>
2025-03-02 11:02:36 +0800

    fix:  重构layer组建

--- src/layout/configLayout/components/rightAside/components/layer/index.tsx
+++ src/layout/configLayout/components/rightAside/components/layer/index.tsx
@@ -1,51 +1,72 @@
 import { FolderOpenOutlined, FolderOutlined } from "@ant-design/icons";
-import { IWidget } from "@src/service";
 import React, { FC } from "react";
 import "./index.scss";
 
 interface IConfigLayoutRightAsideLayer {
-  datas: IWidget[];
+  datas: IAnyObject[];
+  pageId?: string;
   widgetId?: string;
   elementId?: string;
-  onSelected: (widgetId: string, elementId?: string) => void;
+  onSelected: (type: "page" | "widget" | "element", id: string) => void;
 }
+
+const renderTree = (props: IConfigLayoutRightAsideLayer) => {
+  const { datas, pageId, widgetId, elementId, onSelected } = props;
+  return datas.map((item, index) => {
+    return (
+      <div className="cms-config-layout__layer--item" key={index}>
+        <div
+          onClick={() =>
+            onSelected?.(
+              item.pageId ? "page" : item.widgetId ? "widget" : "element",
+              item.pageId || item.widgetId || item.elementId
+            )
+          }
+          className={`name ${
+            (item.pageId === pageId && pageId) ||
+            (item.widgetId === widgetId && widgetId) ||
+            (item.elementId === elementId && elementId)
+              ? "is-active"
+              : ""
+          }`}
+        >
+          {item?.pages?.length ||
+          item?.widgets?.length ||
+          item?.elements?.length ? (
+            <FolderOpenOutlined />
+          ) : (
+            <FolderOutlined />
+          )}{" "}
+          {item.name}
+        </div>
+        {item?.pages?.length ||
+        item?.widgets?.length ||
+        item?.elements?.length ? (
+          <>
+            {renderTree({
+              datas: item?.pages || item.widgets || item.elements,
+              pageId,
+              widgetId,
+              elementId,
+              onSelected,
+            })}
+          </>
+        ) : null}
+      </div>
+    );
+  });
+};
 
 export const ConfigLayoutRightAsideLayer: FC<IConfigLayoutRightAsideLayer> = ({
   datas,
   widgetId,
   elementId,
+  pageId,
   onSelected,
 }) => {
   return (
     <div className="cms-config-layout__layer">
-      {datas.map((widget, index) => (
-        <div
-          key={widget.widgetId || index}
-          className="cms-config-layout__layer--item"
-        >
-          <div
-            className={`name ${
-              widgetId === widget.widgetId ? "is-active" : ""
-            }`}
-            onClick={() => onSelected(widget.widgetId)}
-          >
-            <FolderOpenOutlined /> {widget.name}
-          </div>
-          <div className="content">
-            {widget.elements.map((element) => (
-              <div
-                key={element.elementId}
-                className={`${
-                  elementId === element.elementId ? "is-active" : ""
-                }`}
-                onClick={() => onSelected(widget.widgetId, element.elementId)}
-              >
-                <FolderOutlined /> {element.name}
-              </div>
-            ))}
-          </div>
-        </div>
-      ))}
+      {renderTree({ datas, pageId, widgetId, elementId, onSelected })}
     </div>
   );
 };
