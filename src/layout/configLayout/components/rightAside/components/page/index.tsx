669f5d8db8b604d9ab2f620e8ad97cdabe4e6ece
hejp <378540660@qq.com>
2025-03-03 09:57:17 +0800

    feat: 增加页面配置组件

---
+++ src/layout/configLayout/components/rightAside/components/page/index.tsx
@@ -0,0 +1,43 @@
+import React, { FC } from "react";
+import { Form } from "antd";
+import DynamicForm from "../dynamicForm";
+import { pageConfig } from "@src/core/config/base";
+
+interface IConfigLayoutRightAsidePage {
+  configureValue: IAnyObject;
+  onFinish: (data: IAnyObject) => void;
+}
+
+export const ConfigLayoutRightAsidePage: FC<IConfigLayoutRightAsidePage> = ({
+  configureValue,
+  onFinish,
+}) => {
+  const [form] = Form.useForm();
+  return (
+    <div className="cms-config-layout__element">
+      <Form
+        disabled
+        labelCol={{ flex: "80px" }}
+        labelAlign="left"
+        form={form}
+        initialValues={configureValue}
+      >
+        <DynamicForm
+          datas={pageConfig.configure || []}
+          form={form}
+          callback={(field: string, value: any) => {
+            const val =
+              value && !isNaN(value) && typeof value !== "boolean"
+                ? Number(value)
+                : value;
+            if (configureValue[field] !== val) {
+              onFinish({
+                [field]: val,
+              });
+            }
+          }}
+        />
+      </Form>
+    </div>
+  );
+};
