48497ec7463da202e1e50c545ed758d5d2bbbec6
hejp <378540660@qq.com>
2025-03-04 14:07:55 +0800

    feat: 增加辅助线组件

---
+++ src/layout/gridLayout/components/auxiliaryLine/index.tsx
@@ -0,0 +1,47 @@
+import React, { FC, useMemo } from "react";
+
+interface IAuxiliaryLine {
+  gap: number;
+  column: number;
+  width: number;
+  height: number;
+  row: number;
+}
+
+const AuxiliaryLine: FC<IAuxiliaryLine> = ({
+  gap,
+  column,
+  width,
+  height,
+  row,
+}) => {
+  const style = useMemo(() => {
+    return {
+      display: "grid",
+      gap: `${gap}px`,
+      gridTemplateColumns: `repeat(${column}, ${
+        (width - (column + 1) * gap) / column
+      }px)`,
+      gridTemplateRows: `repeat(${row},  ${
+        (height - (row + 1) * gap) / row
+      }px)`,
+      width: "100%",
+      height: "100%",
+      padding: `${gap}px`,
+    };
+  }, [column, gap, height, row, width]);
+  return (
+    <div className="cms-grid-layout__auxiliaryLine" style={style}>
+      {new Array(column * row).fill(null).map((_, index) => (
+        <div
+          key={index}
+          style={{
+            borderColor: "rgba(255, 255, 255, 0.2)",
+          }}
+        ></div>
+      ))}
+    </div>
+  );
+};
+
+export default AuxiliaryLine;
