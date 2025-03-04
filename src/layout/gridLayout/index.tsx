7b712bf56b06b6c52dede7a8bfbb1466b2ba05dc
hejp <378540660@qq.com>
2025-03-04 17:40:16 +0800

    fix: 处理组件渲染次数问题

--- src/layout/gridLayout/index.tsx
+++ src/layout/gridLayout/index.tsx
@@ -1,5 +1,5 @@
 import { dragStore } from "@src/compoents/dragdrop/drag";
-import React, { FC, ReactNode, useMemo } from "react";
+import React, { memo, ReactNode, useMemo } from "react";
 import ReactGridLayout, { Layout } from "react-grid-layout";
 import "react-grid-layout/css/styles.css";
 import "./index.scss";
@@ -21,94 +21,103 @@ interface IGridLayout {
   onDragStop?: (item: Layout) => void;
 }
 
-const GridLayout: FC<IGridLayout> = ({
-  configureValue = {},
-  groupName = "elements",
-  width = 1080,
-  height = 800,
-  row = 3,
-  column = 4,
-  datas,
-  gap = 10,
-  isDroppable = true,
-  isResizable = true,
-  render,
-  onDrop,
-  onDragStop,
-}) => {
-  const layout = useMemo(() => {
-    let arr: Layout[] = [];
-    arr =
-      datas?.map((item: IAnyObject) => {
-        return {
-          i: item.widgetId || item.elementId,
-          w: item.row,
-          h: item.column,
-          x: item.x,
-          y: item.y,
-        };
-      }) || [];
-    return arr;
-  }, [datas]);
-  return (
-    <div
-      className="cms-grid-layout__main"
-      style={getStyles(configureValue as IAnyObject, "page")}
-    >
-      {isDroppable && isResizable ? (
-        <AuxiliaryLine
-          gap={gap}
-          column={column}
-          width={width}
-          height={height}
-          row={row}
-        />
-      ) : null}
-
-      <ReactGridLayout
-        className="cms-grid-layout"
-        layout={layout}
-        isDroppable={isDroppable}
-        isResizable={isResizable}
-        rowHeight={(height - gap * (row + 1)) / row}
-        maxRows={row}
-        width={width}
-        cols={column}
-        verticalCompact={false}
-        preventCollision
-        margin={[gap, gap]}
-        onDrop={(data, item, e) => {
-          const dragData = dragStore.get(groupName as string);
-          console.log("onDrop");
-          if (item.x <= row && item.y <= column) {
-            onDrop?.(item, dragData);
-          }
-        }}
-        onResizeStop={(data, item, e) => {
-          console.log(data, item, e, "resize");
-        }}
-        onDragStop={(data, oldItem, newItem) => {
-          if (oldItem.x !== newItem.x || oldItem.y !== newItem.y) {
-            onDragStop?.(newItem);
-          }
-        }}
+const GridLayout = memo(
+  (props: IGridLayout) => {
+    const {
+      configureValue = {},
+      groupName = "elements",
+      width = 1080,
+      height = 800,
+      row = 3,
+      column = 4,
+      datas,
+      gap = 10,
+      isDroppable = true,
+      isResizable = true,
+      render,
+      onDrop,
+      onDragStop,
+    } = props;
+    const layout = useMemo(() => {
+      let arr: Layout[] = [];
+      arr =
+        datas?.map((item: IAnyObject) => {
+          return {
+            i: item.widgetId || item.elementId,
+            w: item.row,
+            h: item.column,
+            x: item.x,
+            y: item.y,
+          };
+        }) || [];
+      return arr;
+    }, [datas]);
+    return (
+      <div
+        className="cms-grid-layout__main"
+        style={getStyles(configureValue as IAnyObject, "page")}
       >
-        {datas?.map((item, index) => (
-          <div
-            key={item.widgetId || item.elementId || index}
-            style={{
-              width: "100%",
-              height: "100%",
-              display: "flex",
-              flexDirection: "column",
-            }}
-          >
-            {render?.(item)}
-          </div>
-        ))}
-      </ReactGridLayout>
-    </div>
-  );
-};
+        {isDroppable && isResizable ? (
+          <AuxiliaryLine
+            gap={gap}
+            column={column}
+            width={width}
+            height={height}
+            row={row}
+          />
+        ) : null}
+
+        <ReactGridLayout
+          className="cms-grid-layout"
+          layout={layout}
+          isDroppable={isDroppable}
+          isResizable={isResizable}
+          rowHeight={(height - gap * (row + 1)) / row}
+          maxRows={row}
+          width={width}
+          cols={column}
+          verticalCompact={false}
+          preventCollision
+          margin={[gap, gap]}
+          onDrop={(data, item, e) => {
+            const dragData = dragStore.get(groupName as string);
+            console.log("onDrop");
+            if (item.x <= row && item.y <= column) {
+              onDrop?.(item, dragData);
+            }
+          }}
+          onResizeStop={(data, item, e) => {
+            console.log(data, item, e, "resize");
+          }}
+          onDragStop={(data, oldItem, newItem) => {
+            if (oldItem.x !== newItem.x || oldItem.y !== newItem.y) {
+              onDragStop?.(newItem);
+            }
+          }}
+        >
+          {datas?.map((item, index) => (
+            <div
+              key={item.widgetId || item.elementId || index}
+              style={{
+                width: "100%",
+                height: "100%",
+                display: "flex",
+                flexDirection: "column",
+              }}
+            >
+              {render?.(item)}
+            </div>
+          ))}
+        </ReactGridLayout>
+      </div>
+    );
+  },
+  (prevProps, nextProps) => {
+    if (JSON.stringify(prevProps) !== JSON.stringify(nextProps)) {
+      return false;
+    }
+    return true;
+  }
+);
 
 export default GridLayout;
