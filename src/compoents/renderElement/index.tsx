86666f3af23c8610ae17885d7404f84a1b7a87d1
hejp <378540660@qq.com>
2025-03-15 11:51:52 +0800

    feat: 组件、微件增加延迟加载功能，处理UI页面变大变小问题

--- src/compoents/renderElement/index.tsx
+++ src/compoents/renderElement/index.tsx
@@ -1,4 +1,4 @@
-import React, { FC } from "react";
+import React, { FC, useEffect, useState } from "react";
 import elements from "@src/elements";
 import { capitalizeFirstLetter } from "@src/utils";
 
@@ -8,6 +8,16 @@ interface IRenderElementProps {
 }
 
 const RenderElement: FC<IRenderElementProps> = ({ data, realData }) => {
+  const [isRender, setIsRender] = useState(false);
+  useEffect(() => {
+    const timmer = setTimeout(() => {
+      setIsRender(true);
+    }, 200);
+    return () => {
+      clearTimeout(timmer);
+    };
+  }, []);
+  if (!isRender) return null;
   if (data.element && elements[capitalizeFirstLetter(data.element)]) {
     return React.createElement(elements[capitalizeFirstLetter(data.element)], {
       options: data.configuration.configureValue,
@@ -15,16 +25,19 @@ const RenderElement: FC<IRenderElementProps> = ({ data, realData }) => {
         ? realData || {}
         : data.configuration?.dataValue?.mock,
       field: data.configuration?.dataValue?.field,
-      other: {
-        // 表单变化时向外传递的函数
-        onChangeHandler: (value: any) => {
-          console.log(value, "onChangeHandler");
-        },
-        // 接口参数
-        params: data.configuration?.dataValue?.params || {},
-        // 表单组件值需要保存的字段名，即params接口参数里的字段名
-        paramName: data.configuration?.dataValue?.paramsName,
-      },
+      other:
+        data.type === "form"
+          ? {
+              // 表单变化时向外传递的函数
+              onChangeHandler: (value: any) => {
+                console.log(value, "onChangeHandler");
+              },
+              // 接口参数
+              params: data.configuration?.dataValue?.params || {},
+              // 表单组件值需要保存的字段名，即params接口参数里的字段名
+              paramName: data.configuration?.dataValue?.paramsName,
+            }
+          : null,
     });
   }
   return <div>你访问的组件不存在请联系售后人员</div>;
--- src/compoents/renderWidget/index.tsx
+++ src/compoents/renderWidget/index.tsx
@@ -1,4 +1,4 @@
-import React, { FC } from "react";
+import React, { FC, useEffect, useState } from "react";
 import Request from "@src/compoents/request";
 import { Spin } from "antd";
 import PreviewLayout from "@src/layout/previewLayout";
@@ -13,77 +13,120 @@ import {
   WIDGET_HEADER_GAP,
   WIDGET_HEADER_ROW,
 } from "@src/core/enums/access.enums";
+import { Layout } from "react-grid-layout";
 
 interface IRenderWidgetProps {
   data: IAnyObject;
   configureValue: IAnyObject;
+  isDroppable?: boolean;
+  isResizable?: boolean;
+  staticed?: boolean;
+  selectedId?: string;
+  onDrop?: (item: Layout, data: any, position: "header" | "body") => void;
+  onDragStop?: (item: Layout) => void;
+  onResizeStop?: (item: Layout) => void;
+  onClose?: (item: IAnyObject) => void;
 }
 
-const RenderWidget: FC<IRenderWidgetProps> = ({ data, configureValue }) => {
+const RenderWidget: FC<IRenderWidgetProps> = ({
+  data,
+  configureValue,
+  isDroppable = true,
+  isResizable = true,
+  staticed = true,
+  selectedId,
+  onDrop,
+  onDragStop,
+  onResizeStop,
+  onClose,
+}) => {
+  const [isRender, setIsRender] = useState(false);
+  useEffect(() => {
+    const timmer = setTimeout(() => {
+      setIsRender(true);
+    }, 200);
+    return () => {
+      clearTimeout(timmer);
+    };
+  }, []);
   return (
-    <Request
-      method={data?.configuration?.dataValue?.method}
-      url={data?.configuration?.dataValue?.url}
-      params={JSON.stringify(data?.configuration?.dataValue?.params || {})}
-      render={(loading: boolean, success: boolean, realData: any) => {
-        return (
-          <>
-            {loading ? (
-              <Spin
-                style={{
-                  position: "absolute",
-                  left: "50%",
-                  top: "50%",
-                  transform: "tranlate(-50%, -50%)",
-                  zIndex: 1000,
-                }}
-              />
-            ) : null}
-            <PreviewLayout
-              data={data}
-              header={
-                <GridLayout
-                  datas={
-                    data?.elements.filter(
-                      (item: IElement) => item.position === "header"
-                    ) || []
+    <>
+      {isRender ? (
+        <Request
+          method={data?.configuration?.dataValue?.method}
+          url={data?.configuration?.dataValue?.url}
+          params={JSON.stringify(data?.configuration?.dataValue?.params || {})}
+          render={(loading: boolean, success: boolean, realData: any) => {
+            return (
+              <>
+                {loading ? (
+                  <Spin
+                    style={{
+                      position: "absolute",
+                      left: "50%",
+                      top: "50%",
+                      transform: "tranlate(-50%, -50%)",
+                      zIndex: 1000,
+                    }}
+                  />
+                ) : null}
+                <PreviewLayout
+                  data={data}
+                  header={
+                    <GridLayout
+                      datas={
+                        data?.elements.filter(
+                          (item: IElement) => item.position === "header"
+                        ) || []
+                      }
+                      selectedId={selectedId}
+                      configureValue={configureValue}
+                      column={WIDGET_HEADER_COLUMN}
+                      row={WIDGET_HEADER_ROW}
+                      gap={WIDGET_HEADER_GAP}
+                      render={(data) => (
+                        <RenderElement data={data} realData={realData} />
+                      )}
+                      isDroppable={isDroppable}
+                      isResizable={isResizable}
+                      staticed={staticed}
+                      onDrop={(item, data) => onDrop?.(item, data, "header")}
+                      onDragStop={onDragStop}
+                      onResizeStop={onResizeStop}
+                      onClose={onClose}
+                    />
                   }
-                  configureValue={configureValue}
-                  column={WIDGET_HEADER_COLUMN}
-                  row={WIDGET_HEADER_ROW}
-                  gap={WIDGET_HEADER_GAP}
-                  render={(data) => (
-                    <RenderElement data={data} realData={realData} />
-                  )}
-                  isDroppable
-                  isResizable
-                  staticed
-                />
-              }
-              body={
-                <GridLayout
-                  configureValue={configureValue}
-                  datas={
-                    data?.elements.filter(
-                      (item: IElement) => item.position === "body"
-                    ) || []
+                  body={
+                    <GridLayout
+                      configureValue={configureValue}
+                      datas={
+                        data?.elements.filter(
+                          (item: IElement) => item.position === "body"
+                        ) || []
+                      }
+                      selectedId={selectedId}
+                      column={WIDGET_BODY_COLUMN}
+                      row={WIDGET_BODY_ROW}
+                      gap={WIDGET_BODY_GAP}
+                      render={(data) => (
+                        <RenderElement data={data} realData={realData} />
+                      )}
+                      isDroppable={isDroppable}
+                      isResizable={isResizable}
+                      staticed={staticed}
+                      onDrop={(item, data) => onDrop?.(item, data, "body")}
+                      onDragStop={onDragStop}
+                      onResizeStop={onResizeStop}
+                      onClose={onClose}
+                    />
                   }
-                  column={WIDGET_BODY_COLUMN}
-                  row={WIDGET_BODY_ROW}
-                  gap={WIDGET_BODY_GAP}
-                  render={(data) => (
-                    <RenderElement data={data} realData={realData} />
-                  )}
-                  isDroppable
-                  isResizable
-                  staticed
                 />
-              }
-            />
-          </>
-        );
-      }}
-    />
+              </>
+            );
+          }}
+        />
+      ) : null}
+    </>
   );
 };
 
