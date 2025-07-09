b89296d7142931f80372044245b5ef7be8404be2
hejp <378540660@qq.com>
2025-07-09 11:54:25 +0800

    fix: 测试

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,10 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import { HashRouter } from "react-router-dom";
 import ReactDom from "react-dom/client";
 import App from "./App";
 import "@src/mock";
+
+alert(qq);
 
 const root = ReactDom.createRoot(
   document.getElementById("root") as HTMLElement
