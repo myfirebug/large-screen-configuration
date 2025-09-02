18e033cc34ec46d21e61216ef61574c644a0a5de
hejp <378540660@qq.com>
2025-09-02 10:35:50 +0800

    feat: 增加组件列表国际化

--- src/pages/elements/index.tsx
+++ src/pages/elements/index.tsx
@@ -1,10 +1,11 @@
 import type { ActionType, ProColumns } from "@ant-design/pro-components";
 import { ProTable } from "@ant-design/pro-components";
 
-import React, { useRef, FC, useEffect } from "react";
+import React, { useRef, FC, useEffect, useContext } from "react";
 import { IElement } from "@src/service";
 import { elements } from "@src/core/hook";
 import "./index.scss";
+import { LocaleContext } from "@src/core/i18n/localeContent";
 
 export const waitTimePromise = async (time: number = 100) => {
   return new Promise((resolve) => {
@@ -18,78 +19,81 @@ export const waitTime = async (time: number = 100) => {
   await waitTimePromise(time);
 };
 
-const columns: ProColumns<IElement>[] = [
-  {
-    title: "名称",
-    dataIndex: "name",
-  },
-  {
-    title: "图片",
-    dataIndex: "url",
-    search: false,
-    render: (text, record, _, action) => {
-      return <img height="60px" src={`./elements/${record.code}.png`} alt="" />;
-    },
-  },
-  {
-    title: "类型",
-    dataIndex: "type",
-    onFilter: true,
-    ellipsis: true,
-    valueType: "select",
-    valueEnum: {
-      all: { text: "全部", status: "" },
-      text: {
-        text: "文本",
-        status: "text",
-      },
-      image: {
-        text: "图片",
-        status: "image",
-      },
-      table: {
-        text: "表格",
-        status: "table",
-      },
-      line: {
-        text: "折线图",
-        status: "line",
-      },
-      bar: {
-        text: "柱状图",
-        status: "bar",
-      },
-      pie: {
-        text: "饼图",
-        status: "pie",
-      },
-    },
-  },
-  {
-    title: "组件",
-    dataIndex: "element",
-    search: false,
-  },
-  {
-    title: "使用次数",
-    dataIndex: "count",
-    search: false,
-  },
-  {
-    title: "创建时间",
-    dataIndex: "createTime",
-    valueType: "date",
-    hideInSearch: true,
-  },
-  {
-    title: "创建时间",
-    dataIndex: "createTime",
-    valueType: "dateRange",
-    hideInTable: true,
-  },
-];
-
 const Elements: FC<any> = () => {
+  const { $t } = useContext(LocaleContext);
+
+  const columns: ProColumns<IElement>[] = [
+    {
+      title: $t("element_name" /*名称*/),
+      dataIndex: "name",
+    },
+    {
+      title: $t("element_img" /*图片*/),
+      dataIndex: "url",
+      search: false,
+      render: (text, record, _, action) => {
+        return (
+          <img height="60px" src={`./elements/${record.code}.png`} alt="" />
+        );
+      },
+    },
+    {
+      title: $t("element_type" /*类型*/),
+      dataIndex: "type",
+      onFilter: true,
+      ellipsis: true,
+      valueType: "select",
+      valueEnum: {
+        all: { text: $t("element_all" /*全部*/), status: "" },
+        text: {
+          text: $t("element_text" /*文本*/),
+          status: "text",
+        },
+        image: {
+          text: $t("element_img" /*图片*/),
+          status: "image",
+        },
+        table: {
+          text: $t("element_table" /*表格*/),
+          status: "table",
+        },
+        line: {
+          text: $t("element_line" /*折线图*/),
+          status: "line",
+        },
+        bar: {
+          text: $t("element_bar" /*柱状图*/),
+          status: "bar",
+        },
+        pie: {
+          text: $t("element_pie" /*饼图*/),
+          status: "pie",
+        },
+      },
+    },
+    {
+      title: $t("element_component" /*组件*/),
+      dataIndex: "element",
+      search: false,
+    },
+    {
+      title: $t("element_use_num" /*使用次数*/),
+      dataIndex: "count",
+      search: false,
+    },
+    {
+      title: $t("element_create_time" /*创建时间*/),
+      dataIndex: "createTime",
+      valueType: "date",
+      hideInSearch: true,
+    },
+    {
+      title: $t("element_create_time" /*创建时间*/),
+      dataIndex: "createTime",
+      valueType: "dateRange",
+      hideInTable: true,
+    },
+  ];
   const actionRef = useRef<ActionType>();
   const { filterElementsList, getElements, filterHandle } = elements();
 
@@ -122,7 +126,7 @@ const Elements: FC<any> = () => {
           onChange: (page) => console.log(page),
         }}
         dateFormatter="string"
-        headerTitle="组件列表"
+        headerTitle={$t("element_components_list" /*组件列表*/)}
       />
     </div>
   );
